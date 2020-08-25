69a01c07f44842fce74c6fe563167be647c2de31
DrixnFS <krzyzanek.martin@gmail.com>
2020-08-25 23:19:21 +0200

    basic files and structe

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import UserStore from './stores/UserStore';
+import LoginForm from './components/LoginForm';
+import LogoutForm from './components/LogoutForm';
+import './App.css';
+
+class App extends React.Component {
+
+	componentDidMount(){
+		try{
+			
+		} catch(err){
+
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{/* TODO: logic behind loginform and logout form */}
+			</div>
+		)
+	}; 
+}
+
+export default App;
---
+++ src/components/LogoutForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LogoutForm extends React.Component {
+
+	render() {
+		return (
+			<div className="logoutForm">
+
+			</div>
+		)
+	}; 
+}
+
+export default LogoutForm;
